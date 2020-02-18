6b7266f0d31594d2baacbf82589457ff018049d3
Sergey Ivanenko <svivanenko@avito.ru>
2020-02-18 08:37:23 +0300

    usecase

--- internal/demo_app/usecase/usecase_user.go
+++ internal/demo_app/usecase/usecase_user.go
@@ -1,6 +1,9 @@
 package usecase_user
 
-import "github.com/blac3kman/Innopolis/internal/demo_app/entities"
+import (
+	"github.com/blac3kman/Innopolis/internal/demo_app/entities"
+	"github.com/blac3kman/Innopolis/internal/demo_app/repository"
+)
 
 type (
 	//mockery -name=User
@@ -10,25 +13,29 @@ type (
 		UpdateEmail(id int64, email string) (entities.User, error)
 		Delete(id int64) error
 	}
-	usecase struct {}
+	usecase struct {
+		repo repository.User
+	}
 )
 
-func New() *usecase {
-	return &usecase{}
+func New(repo repository.User) *usecase {
+	return &usecase{
+		repo: repo,
+	}
 }
 
-func (u usecase) Get(id int64) (entities.User, error) {
-	return entities.User{}, nil
+func (u *usecase) Get(id int64) (entities.User, error) {
+	return u.repo.Read(id)
 }
 
-func (u usecase) Create(name, email string) (entities.User, error) {
-	return entities.User{}, nil
+func (u *usecase) Create(name, email string) (entities.User, error) {
+	return u.repo.Create(name, email)
 }
 
-func (u usecase) UpdateEmail(id int64, email string) (entities.User, error) {
-	return entities.User{}, nil
+func (u *usecase) UpdateEmail(id int64, email string) (entities.User, error) {
+	return u.repo.UpdateEmail(id, email)
 }
 
-func (u usecase) Delete(id int64) error {
-	return nil
+func (u *usecase) Delete(id int64) error {
+	return u.repo.Delete(id)
 }
